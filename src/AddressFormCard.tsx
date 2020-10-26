d0f8e799e6f297e5bf09bfa4409beea8b9e58f8f
Brian Murphy <smurph02@gmail.com>
2020-10-26 16:15:21 -0700

    General functionality and styling for components
    removed logs
    Hide components when entering a different address in the main input. Also handle keydown in secondary address field
    Removed unnecessary comment

--- src/AddressFormCard.tsx
+++ src/AddressFormCard.tsx
@@ -2,6 +2,11 @@ import { Activity, CardAction, DirectLineOptions, Message} from 'botframework-di
 import * as moment from 'moment';
 import * as React from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng
+} from 'react-places-autocomplete';
 import { connect } from 'react-redux';
 import { ChatState } from './Store';
 import { ChatActions, sendMessage } from './Store';
@@ -27,6 +32,17 @@ export interface AddressFormState {
   addressError: string;
   loaded: boolean;
   suggestionsShown: boolean;
+  addressComponents: AddressComponents;
+}
+
+export interface AddressComponents {
+  street_address_1: string;
+  street_address_2: string;
+  city: string;
+  county: string;
+  state: string;
+  zipcode: string;
+  country: string;
 }
 
 class AddressForm extends React.Component<AddressFormProps, AddressFormState> {
@@ -40,7 +56,16 @@ class AddressForm extends React.Component<AddressFormProps, AddressFormState> {
       addressSelected: '',
       addressError: undefined,
       loaded: false,
-      suggestionsShown: false
+      suggestionsShown: false,
+      addressComponents: {
+        street_address_1: undefined,
+        street_address_2: undefined,
+        city: undefined,
+        county: undefined,
+        state: undefined,
+        zipcode: undefined,
+        country: undefined
+      }
     };
   }
 
@@ -81,27 +106,97 @@ class AddressForm extends React.Component<AddressFormProps, AddressFormState> {
     return validated;
   }
 
+  formatAddressFromComponents = () => {
+    let address = this.state.addressComponents.street_address_1;
+    if (!!this.state.addressComponents.street_address_2) {
+      address = `${address}, ${this.state.addressComponents.street_address_2}`;
+    }
+    address = `${address}, ${this.state.addressComponents.city}`;
+    address = `${address}, ${this.state.addressComponents.state}`;
+    address = `${address}, ${this.state.addressComponents.zipcode}`;
+    address = `${address}, ${this.state.addressComponents.country}`;
+
+    return address;
+  }
+
   clickToSubmitAddressInformation(e: React.MouseEvent<HTMLButtonElement>) {
     if (!this.validateAddress()) { return; }
 
     this.resetShell();
-    this.props.sendMessage(this.state.addressSelected);
+    this.props.sendMessage(this.formatAddressFromComponents());
 
     e.stopPropagation();
   }
 
+  handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter' && this.validateAddress()) {
+      this.resetShell();
+      this.props.sendMessage(this.formatAddressFromComponents());
+    }
+  }
+
   handleAddressChange = (value: string) => {
-    this.setState({ addressTyped: value, addressSelected: '' });
+    this.setState({
+      ...this.state,
+      addressSelected: '',
+      addressTyped: value,
+      addressComponents: {
+        street_address_1: undefined,
+        street_address_2: undefined,
+        city: undefined,
+        county: undefined,
+        state: undefined,
+        zipcode: undefined,
+        country: undefined
+      }
+    });
+  }
+
+  getComponentFromGeocodeResult = (components: google.maps.GeocoderAddressComponent[], key: string) => {
+    const componentObject = components.find(c => c.types.includes(key));
+    return componentObject && componentObject.long_name ? componentObject.long_name : undefined;
   }
 
   handleAddressSelect = (address: string, placeId: string) => {
     if (this.state.addressSelected === address) {
       this.resetShell();
-      this.props.sendMessage(this.state.addressSelected);
+      this.props.sendMessage(this.formatAddressFromComponents());
       return;
     }
 
-    this.setState({...this.state, addressSelected: address, addressTyped: address});
+    geocodeByPlaceId(placeId)
+      .then(results => {
+        if (results.length > 0) {
+          const components = results[0].address_components;
+
+          const streetNumber = this.getComponentFromGeocodeResult(components, 'street_number');
+          const street = this.getComponentFromGeocodeResult(components, 'route');
+          const city = this.getComponentFromGeocodeResult(components, 'locality');
+          const county = this.getComponentFromGeocodeResult(components, 'administrative_area_level_2');
+          const state = this.getComponentFromGeocodeResult(components, 'administrative_area_level_1');
+          const zip = this.getComponentFromGeocodeResult(components, 'postal_code');
+          const zipSuffix = this.getComponentFromGeocodeResult(components, 'postal_code_suffix');
+          const country = this.getComponentFromGeocodeResult(components, 'country');
+
+          const zipFull = !!zipSuffix ? `${zip}-${zipSuffix}` : zip;
+
+          this.setState({
+            ...this.state,
+            addressSelected: address,
+            addressTyped: address,
+            addressComponents: {
+              street_address_1: `${streetNumber} ${street}`,
+              street_address_2: undefined,
+              city,
+              county,
+              state,
+              zipcode: zipFull,
+              country
+            }
+          });
+        }
+      })
+      .catch(error => console.error('geocode error', error));
   }
 
   scrollToBottom = () => {
@@ -148,6 +243,52 @@ class AddressForm extends React.Component<AddressFormProps, AddressFormState> {
           </div>
         )}
         </PlacesAutocomplete>}
+        {this.state.addressComponents.street_address_1 && <div className="address__form__card__components">
+          <span>Street Address</span>
+          <input
+            className="address__form__card__components__street__1"
+            value={this.state.addressComponents.street_address_1}
+            readOnly
+          />
+          <span>Unit/Apartment</span>
+          <input
+            className="address__form__card__components__street__2"
+            value={this.state.addressComponents.street_address_2}
+            onKeyDown={e => this.handleKeyPress(e)}
+            onChange={e => this.setState({
+                ...this.state,
+                addressComponents: {
+                  ...this.state.addressComponents,
+                  street_address_2: e.target.value
+                }
+              })
+            }
+          />
+          <span>City</span>
+          <input
+            className="address__form__card__components__city"
+            value={this.state.addressComponents.city}
+            readOnly
+          />
+          <span>State/Province</span>
+          <input
+            className="address__form__card__components__state"
+            value={this.state.addressComponents.state}
+            readOnly
+          />
+          <span>Postal Code</span>
+          <input
+            className="address__form__card__components__postal__code"
+            value={this.state.addressComponents.zipcode}
+            readOnly
+          />
+          <span>Country</span>
+          <input
+            className="address__form__card__components__country"
+            value={this.state.addressComponents.country}
+            readOnly
+          />
+        </div>}
         <button className="address__form__card__submit" onClick={e => this.clickToSubmitAddressInformation(e)}>Submit</button>
         <div ref={el => this.bottomDiv = el}></div>
       </div>
