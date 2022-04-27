5b434661966d1bb46dec1efb0f0bff5249b10e7c
Jason Greaves <jgreaves@sandboxunion.com>
2022-04-27 16:40:17 -0400

    work on address card should be done, just gotta figure out how to get the bot to use it

--- src/AddressCard.tsx
+++ src/AddressCard.tsx
@@ -1,6 +1,7 @@
-import { Activity, CardAction, DirectLine, DirectLineOptions, Message} from 'botframework-directlinejs';
+import { Activity, CardAction, DirectLine, DirectLineOptions, Message } from 'botframework-directlinejs';
 import * as moment from 'moment';
 import * as React from 'react';
+import Autocomplete from 'react-google-autocomplete';
 import { connect } from 'react-redux';
 import { activityWithSuggestedActions } from './activityWithSuggestedActions';
 import { doCardAction, IDoCardAction } from './Chat';
@@ -8,57 +9,136 @@ import { FormattedText } from './FormattedText';
 import { filteredActivities } from './History';
 import { ChatState } from './Store';
 import { ChatActions, sendMessage } from './Store';
+import { defaultStrings } from './Strings';
 
 export interface Node {
-  node_type: string;
-  meta: any;
+    node_type: string;
+    meta: any;
 }
 
 interface AddressProps {
-  node: Node;
-  sendMessage: (inputText: string) => void;
-  directLine?: DirectLineOptions;
-  gid: string;
-  conversationId: string;
-  withTime: boolean;
-  updateInput: (disable: boolean, placeholder: string) => void;
+    node: Node;
+    sendMessage: (inputText: string) => void;
+    directLine?: DirectLineOptions;
+    gid: string;
+    conversationId: string;
+    withTime: boolean;
+    updateInput: (disable: boolean, placeholder: string) => void;
 }
 
 export interface MessageWithAddress extends Message {
-    address: string
+    address: string;
 }
 
 export interface AddressState {
     address: string;
-    addressError: string; 
+    addressError: string;
     formattedMessage: string;
-  }
+}
 
-  class AddressForm extends React.Component<AddressProps, AddressState> {
+class AddressForm extends React.Component<AddressProps, AddressState> {
     private textInputAddress: HTMLInputElement;
-  }
 
-  export const AddressCard = connect(
+    constructor(props: AddressProps) {
+        super(props);
+
+        this.state = {
+            address: '',
+            addressError: undefined,
+            formattedMessage: ''
+        };
+
+        console.log(this.props);
+
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.updateInput(
+            true,
+            'Please enter your address.'
+        );
+    }
+
+    getFormattedAddress = () => {
+        return JSON.stringify({
+            ...this.state.address && { email: this.state.address }
+
+        });
+    }
+
+    private handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>): any {
+        if (e.key === 'Enter') {
+            this.resetShell();
+            this.props.sendMessage(this.getFormattedAddress());
+            document.removeEventListener('keypress', this.handleKeyDown.bind(this));
+        }
+    }
+
+    resetShell = () => {
+        this.props.updateInput(
+            false,
+            defaultStrings.consolePlaceholder
+        );
+    }
+
+    private onKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            this.resetShell();
+            this.props.sendMessage(this.getFormattedAddress());
+            document.removeEventListener('keypress', this.handleKeyDown.bind(this));
+        }
+    }
+
+    render() {
+        return (
+            <div className="address__card">
+                <div className="address__card__container">
+                    <span className={'address__card__container__title'}>Name</span>
+                    <input
+                        type="text"
+                        className={'address__card__container__input'}
+                        ref={input => this.textInputAddress = input}
+                        autoFocus={true}
+                        value={this.state.address}
+                        onChange={e => this.setState({
+                            ...this.state,
+                            address: e.target.value
+                        })}
+                        onKeyPress={e => this.onKeyPress(e)}
+                        // onFocus={ () => this.onTextInputFocus() }
+                        placeholder="Address"
+                        aria-label={null}
+                        aria-live="polite"
+                    />
+                    {this.state.addressError && <span className="address__card__container__error">{this.state.addressError}</span>}
+                </div>
+            </div>
+        );
+    }
+}
+
+export const AddressCard = connect(
     (state: ChatState) => {
-      return {
-        // passed down to MessagePaneView
-        locale:       state.format.locale,
-        user: state.connection.user,
-        conversationId: state.connection.botConnection.conversationId
-      };
+        return {
+            // passed down to MessagePaneView
+            locale: state.format.locale,
+            user: state.connection.user,
+            conversationId: state.connection.botConnection.conversationId
+        };
     }, {
-      updateInput: (disable: boolean, placeholder: string) =>
-            ({
-                type: 'Update_Input',
-                placeholder,
-                disable,
-                source: 'text'
-            } as ChatActions),
-      sendMessage
-    }, (stateProps: any, dispatchProps: any, ownProps: any): AddressProps => {
-      return {
+    updateInput: (disable: boolean, placeholder: string) =>
+    ({
+        type: 'Update_Input',
+        placeholder,
+        disable,
+        source: 'text'
+    } as ChatActions),
+    sendMessage
+}, (stateProps: any, dispatchProps: any, ownProps: any): AddressProps => {
+    return {
         // from stateProps
-        node:       ownProps.node,
+        node: ownProps.node,
         withTime: ownProps.withTime,
         // from dispatchProps
         sendMessage: (text: string) => dispatchProps.sendMessage(text, stateProps.user, stateProps.locale),
@@ -66,6 +146,6 @@ export interface AddressState {
         directLine: ownProps.directLine,
         conversationId: stateProps.conversationId,
         updateInput: dispatchProps.updateInput
-      };
-    }
-  )(AddressForm);
+    };
+}
+)(AddressForm);
