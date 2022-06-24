fcbaab48b9a1ffba0e2b6f166e383ff48112d08b
Dana <dcolletti@sandboxunion.com>
2022-06-24 16:57:13 -0400

    header logo, text, and "x" icon scaled down

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -572,7 +572,7 @@ export class Chat extends React.Component<ChatProps, State> {
 
                                   <span>{typeof state.format.chatTitle === 'string' ? state.format.chatTitle : 'Gideon' }</span>
                                     {/* Close X image on chat */}
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => {this.toggle(); }} >
+                                    <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => {this.toggle(); }} >
                                         <title>wc-header--close</title>
                                         <path className="wc-header--close" d="M18 2L2 18" stroke="#FCFCFC" stroke-width="3" stroke-linecap="round" />
                                         <path className="wc-header--close" d="M2 2L18 18" stroke="#FCFCFC" stroke-width="3" stroke-linecap="round" />
