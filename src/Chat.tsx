649affadf3427844e96da48717d6bdaa4b8460c5
jtalavera <jtalavera@sandboxunion.com>
2022-12-15 16:48:06 -0500

    added chataction

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -253,6 +253,7 @@ export class Chat extends React.Component<ChatProps, State> {
                     type: 'Set_Messages',
                     activities: activitiesCopy
                 });
+                this.store.dispatch<ChatActions>({ type: activity.from.id === state.connection.user.id ? 'Receive_Sent_Message' : 'Receive_Message', activity });
             }
         } else {
             console.log("else statement")
