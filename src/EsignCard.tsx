b5dbce028c0a0fd8e4cb07ab9f7fa1e1fd908924
Raven <xyjiang@sandboxunion.com>
2023-03-09 17:07:33 -0500

    Mobile Modal WIP

--- src/EsignCard.tsx
+++ src/EsignCard.tsx
@@ -363,9 +363,15 @@ class Esign extends React.Component<EsignProps, EsignState> {
         console.log("in new modal")
         return (
             <div className='fullview'>
-                 <div className='test'>
-                mobile modal here
-            </div> 
+                <div className='test'>
+                <div className="mobile-modal">
+                    <input className="esign-initial-box" placeholder="Initial Here" type="text" value={this.state.initials} onKeyPress={this.handleKeyDown} onChange={this.onChangeInitials} id="initial"></input>
+                    <input className="esign-input-box" placeholder="Type in Full Name to Create Signature" type="text" value={this.state.signature} onKeyPress={this.handleKeyDown} onChange={this.onChangeSignature} id="signature"></input>
+                <div className="button-area">
+                    <button  id="sign-btn" className="gideon-submit-button" style={{width: "80%" }} onClick={e => this.clickToSubmitSignature(e)}> SIGN </button>
+                </div>
+            </div>
+                </div> 
             </div>)
     }
 
