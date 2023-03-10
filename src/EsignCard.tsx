f1243e9a8076548239acb144beb550dec2dbba46
Audrey Rovero <arovero@sandboxunion.com>
2023-03-10 13:37:30 -0500

    Some small fixes

--- src/EsignCard.tsx
+++ src/EsignCard.tsx
@@ -517,10 +517,10 @@ class Esign extends React.Component<EsignProps, EsignState> {
         let sig = (
             <footer className="signature-box-area">
             <div className="submit-area">
-                <input className="esign-initial-box" placeholder="Initial Here" type="text" value={this.state.initials} onKeyPress={this.handleKeyDown} onChange={this.onChangeInitials} id="initial"></input>
+                <input className="esign-initial-box" placeholder="Your Initials" type="text" value={this.state.initials} onKeyPress={this.handleKeyDown} onChange={this.onChangeInitials} id="initial"></input>
                 <input className="esign-input-box" placeholder="Type in Full Name to Create Signature" type="text" value={this.state.signature} onKeyPress={this.handleKeyDown} onChange={this.onChangeSignature} id="signature"></input>
                 <div className="button-area">
-                    <button  id="sign-btn" className="gideon-submit-button" style={{width: "80%" }} onClick={e => this.clickToSubmitSignature(e)}> SIGN </button>
+                    <button  id="sign-btn" className="gideon-submit-button" style={{width: "80%" }} onClick={e => this.clickToSubmitSignature(e)}> Sign Now  </button>
                 </div>
             </div>
            </footer>   
