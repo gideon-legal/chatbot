9d26fe2aa901d5ebd5e193ab065bde3fba18e863
Audrey Rovero <arovero@sandboxunion.com>
2023-05-01 15:10:24 -0400

    Added if cases in post esign render

--- src/EsignCardReadOnly.tsx
+++ src/EsignCardReadOnly.tsx
@@ -41,48 +41,97 @@ export class EsignCardReadOnly extends React.Component<EsignCardReadOnlyProps, {
 
     //change list view to button
     render() {
-        if(document.getElementById('btn3') != null){
-            document.getElementById('btn3').style.display = "none"
-        }
-        if(document.getElementById("presignnode")!= null){
-           document.getElementById("presignnode").style.display = "none"
-          
-        }
-        return (
-            <div className="modal-normal">
-            <div className="modal-content">
-            <div className="esign__card gideon__node">
-                <div>
-                
-                    <div className="document_area2">
-                        <EsignNode />
-                    </div>
-                    <div className='esign-message-handoff-big2'>
-                        Congrats! 
-                    </div>
-                    <div className="esign-message-handoff-small2">
-                    {this.props.post_message.postesign_message ||
-                 "Please download your completed representation agreement below. A member of our team will be in touch to advise you on your next steps. Thank you!"}
-                      
-                    </div>
-                <div className="uploaded-files-container2">
-                <div >
-                    {this.props.files.length <= 0 ? <a className="gideon-submit-button-download" target="_blank" href={sessionStorage.getItem("file")}>{"Download Document"}</a> :
-                    this.props.files.map((file: any) => (
-                        <a className="gideon-submit-button-download" target="_blank" href={file.url}>{"Download Document"}</a>
-                     
-                  ))
-                    }
+        if(this.props.fullscreen != false){
+            // do fullscreen version -> modify srtyling
+            if(document.getElementById('btn3') != null){
+                document.getElementById('btn3').style.display = "none"
+            }
+            if(document.getElementById("presignnode")!= null){
+               document.getElementById("presignnode").style.display = "none"
               
+            }
+            return (
+                <div className="modal-normal">
+                <div className="modal-content">
+                <div className="esign__card gideon__node">
+                    <div>
+                    
+                        <div className="document_area2">
+                            <EsignNode />
+                        </div>
+                        <div className='esign-message-handoff-big2'>
+                            Congrats! 
+                        </div>
+                        <div className="esign-message-handoff-small2">
+                        {this.props.post_message.postesign_message ||
+                     "Please download your completed representation agreement below. A member of our team will be in touch to advise you on your next steps. Thank you!"}
+                          
+                        </div>
+                    <div className="uploaded-files-container2">
+                    <div >
+                        {this.props.files.length <= 0 ? <a className="gideon-submit-button-download" target="_blank" href={sessionStorage.getItem("file")}>{"Download Document"}</a> :
+                        this.props.files.map((file: any) => (
+                            <a className="gideon-submit-button-download" target="_blank" href={file.url}>{"Download Document"}</a>
+                         
+                      ))
+                        }
+                  
+                    </div>
                 </div>
-            </div>
+                    </div>
                 </div>
-            </div>
-
-            </div>
-           
-            </div>
-        )
+    
+                </div>
+               
+                </div>
+            )
+        } else {
+            // not fullscreen styling
+            if(document.getElementById('btn3') != null){
+                document.getElementById('btn3').style.display = "none"
+            }
+            if(document.getElementById("presignnode")!= null){
+               document.getElementById("presignnode").style.display = "none"
+              
+            }
+            return (
+                <div className="modal-normal">
+                <div className="modal-content">
+                <div className="esign__card gideon__node">
+                    <div>
+                    
+                        <div className="document_area2">
+                            <EsignNode />
+                        </div>
+                        <div className='esign-message-handoff-big2'>
+                            Congrats! 
+                        </div>
+                        <div className="esign-message-handoff-small2">
+                        {this.props.post_message.postesign_message ||
+                     "Please download your completed representation agreement below. A member of our team will be in touch to advise you on your next steps. Thank you!"}
+                          
+                        </div>
+                    <div className="uploaded-files-container2">
+                    <div >
+                        {this.props.files.length <= 0 ? <a className="gideon-submit-button-download" target="_blank" href={sessionStorage.getItem("file")}>{"Download Document"}</a> :
+                        this.props.files.map((file: any) => (
+                            <a className="gideon-submit-button-download" target="_blank" href={file.url}>{"Download Document"}</a>
+                         
+                      ))
+                        }
+                  
+                    </div>
+                </div>
+                    </div>
+                </div>
+    
+                </div>
+               
+                </div>
+            )
+            
+        }
+       
     }
 }
 
